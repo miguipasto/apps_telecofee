ebddac44776d15e96da4329080a1a071d7a2b14b
Tu Nombre <you@example.com>
2024-02-25 13:07:04 +0100

    Proyectos

--- TeleCoffee_Clients/capacitor.config.ts
+++ TeleCoffee_Clients/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'TeleCoffe_Workers',
+  appName: 'TeleCoffee_Clients',
   webDir: 'www',
   server: {
     androidScheme: 'https'
--- TeleCoffee_Workers/capacitor.config.ts
+++ TeleCoffee_Workers/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'TeleCoffe_Clients',
+  appName: 'TeleCoffee_Workers',
   webDir: 'www',
   server: {
     androidScheme: 'https'
