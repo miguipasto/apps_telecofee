e859cec24cdc418cc1b3d1cdbda90c67bf7531da
Miguel Pastoriza <miguelpastoriza2002@gmail.com>
2024-03-18 22:48:53 +0100

    CSS fix

---
+++ TeleCoffe_Workers/src/app/main/profile/profile.page.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import { DataService } from 'src/app/services/data.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.page.html',
+  styleUrls: ['./profile.page.scss'],
+})
+export class ProfilePage implements OnInit {
+  cadena: string = "";
+  isOverlayVisible: boolean = false;
+  formPassword!: FormGroup;
+  datosUser: any = [];
+
+  constructor(
+    public authService: UserService,
+    public route: Router,
+    private formBuilder: FormBuilder,
+    private dataService: DataService) { }
+
+  async ngOnInit() {  
+    this.formPassword = new FormGroup({
+      currentPassword: new FormControl('', Validators.required),
+      newPassword: new FormControl('', Validators.required)
+    });
+
+    this.datosUser = await this.dataService.obetenerDatosUsuario();
+  }
+
+
+  showOverlay(cadena: string) {
+    if (cadena.includes('contraseña')) {
+
+      console.log("Nueva " + this.formPassword.value.newPassword);
+      console.log("Actual " + this.formPassword.value.currentPassword);
+  
+      if (this.formPassword.invalid) {
+        const currentPasswordErrors = this.formPassword?.get('currentPassword')?.errors;
+        let errorMessage = 'Please fill in all fields or fix errors.';
+        if (currentPasswordErrors) {
+          errorMessage = Object.keys(currentPasswordErrors)[0] === 'required'
+            ? 'Current password is required.'
+            : 'There are errors in current password.';
+        }
+        alert(errorMessage);
+        this.formPassword.reset();
+        return;
+      }
+    }
+    this.isOverlayVisible = true;
+    this.cadena = cadena;
+  }
+  
+  
+  async Confirm(cadena: string) {
+    if (cadena.includes('Eliminar')) {
+      await this.authService.deleteAccount().then(() => {
+        console.log('Cuenta eliminada');
+        this.route.navigate(['login']);
+      }).catch(error => {
+        console.error('Error al eliminar la cuenta', error);
+      });
+    }else{
+      const np=this.formPassword.value.currentPassword
+      const op=this.formPassword.value.newPassword
+
+      this.authService.reauthenticateWithCredential(np,op);
+      this.formPassword.reset();
+
+
+    }
+    this.isOverlayVisible = false;
+  }
+  
+
+  Cancel() {
+    this.isOverlayVisible = false;
+  }
+
+  LogOut(){
+    this.authService.signOut();
+    this.route.navigate(['login'])
+  }
+
+}
+
