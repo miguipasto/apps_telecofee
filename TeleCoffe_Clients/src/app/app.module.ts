31fd43cb571a306815ff82cef80dcbbd49cab4f0
Miguel Pastoriza <miguelpastoriza2002@gmail.com>
2024-03-29 12:29:24 +0100

    Cambios fotos

--- TeleCoffe_Clients/src/app/app.module.ts
+++ TeleCoffe_Clients/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
