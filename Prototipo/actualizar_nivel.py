acf6589db6eea4b68b4e5b8d310f2badfe697caa
Miguel Pasoriza <you@example.com>
2024-04-14 17:30:44 +0200

    Directorio

--- Prototipo/actualizar_nivel.py
+++ Prototipo/actualizar_nivel.py
@@ -12,7 +12,7 @@ maquinas = ["teleco"]
 mensajes_recibidos = {}
 
 # Configuración Firebase
-credenciales_workers = credentials.Certificate('/home/lpro/apps_telecofee/Simulacion/lpro-workers-firebase-adminsdk-yf4f8-595b4803e9.json')
+credenciales_workers = credentials.Certificate('/home/lpro/LPRO/apps_telecofee/Simulacion/lpro-workers-firebase-adminsdk-yf4f8-595b4803e9.json')
 app_workers = firebase_admin.initialize_app(credenciales_workers, name='workers')
 db_workers = firestore.client(app_workers)
 
