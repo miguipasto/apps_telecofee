d376fc1d0b27fb89c4595224605b99b118d07695
Miguel pastoriza <miguelpastoriza2002@gmail.com>
2024-04-29 18:41:54 +0200

    Arduino code

--- Prototipo/sensores/sensores.ino
+++ Prototipo/sensores/sensores.ino
@@ -6,7 +6,8 @@
 #endif
 
 const int sensor1 = 6;
-const int sensor2 = 7;
+const int sensor2trig = 12;
+const int sensor2echo = 13;
 const int fototransistor = 2;
 double distancia1, distancia2;
 boolean fototransistor_activo = false;
@@ -19,13 +20,16 @@ HX711_ADC LoadCell(HX711_dout, HX711_sck);
 const int calVal_eepromAddress = 0;
 
 Ping ping1 = Ping(sensor1);
-Ping ping2 = Ping(sensor2);
+//Ping ping2 = Ping(sensor2);
 
 void setup() {
   Serial.begin(57600);
   delay(10);
   Serial.println("Starting...");
   
+  pinMode(sensor2trig, OUTPUT);
+  pinMode(sensor2echo, INPUT);
+  digitalWrite(sensor2trig, LOW);
   pinMode(fototransistor, INPUT);
   attachInterrupt(digitalPinToInterrupt(fototransistor), activo, RISING);
   
@@ -59,6 +63,8 @@ void measureAndReport() {
   float peso_actual = 0;
   float peso_anterior = peso_actual;       // Inicializa peso_anterior con el mismo valor para la primera comparación
   int measurementCount = 0;
+  float time = 0;
+  float dist = 0;
 
   // Bucle de medición para estabilizar la lectura del peso
   while (abs(peso_actual - peso_anterior) > 0.50 || measurementCount < 5) {
@@ -74,8 +80,12 @@ void measureAndReport() {
   // Lecturas del sensor ultrasónico
   ping1.fire();
   distancia1 = ping1.centimeters();
-  ping2.fire();
-  distancia2 = ping2.centimeters();
+  digitalWrite(sensor2trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(sensor2trig, LOW);
+
+  time = pulseIn(sensor2echo, HIGH);
+  dist = time/59;
 
   // Salida de datos estructurados en formato JSON
   Serial.print(F("{ \"peso_gr\": "));
@@ -83,20 +93,20 @@ void measureAndReport() {
   Serial.print(F(", \"sensor_1_cm\": "));
   Serial.print(distancia1);
   Serial.print(F(", \"sensor_2_cm\": "));
-  Serial.print(distancia2);
+  Serial.print(dist);
   Serial.println(F(" }"));
 
-  if (fototransistor_activo == true){
-    fototransistor_activo = false;
-    Serial.println("Fototransistor: 1");
-    delay(200);
+  //if (fototransistor_activo == true){
+  //  fototransistor_activo = false;
+  //  Serial.println("Fototransistor: 1");
+  //  delay(200);
 
-  }
+  //}
 }
 
 
 void activo() {
   //Serial.println("Fototransistor: 1");
-
-  fototransistor_activo = true;
+  Serial.println("Fototransistor: 1");
+  //fototransistor_activo = true;
 }
